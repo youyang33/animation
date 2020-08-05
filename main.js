8998cdd2077ee38be65ae0a43dc8c19fb28fd949
youyang33 <ouyanghkx@gmail.com>
2020-08-04 20:04:36 -0500

    Line Drawing Icons
    Anime.js

---
+++ main.js
@@ -0,0 +1,70 @@
+
+
+anime({
+  targets: '#outline2',
+  strokeDashoffset: [anime.setDashoffset, 0],
+   easing: 'easeInOutSine',
+  duration: 1500,
+  loop: true,
+  delay: function(el, i) { return i * 250 },
+});
+
+
+
+anime({
+  targets: '#outline',
+  strokeDashoffset: [anime.setDashoffset, 0],
+   easing: 'easeInOutSine',
+  duration: 1500,
+  loop: true,
+  delay: function(el, i) { return i * 200 },
+});
+
+
+
+
+anime({
+  targets: '#outline3',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  loop: true,
+  delay: function(el, i) { return i * 200 },
+});
+
+anime({
+  targets: '#outline4',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  loop: true,
+  delay: function(el, i) { return i * 250 },
+});
+
+
+anime({
+  targets: '#outline5',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  loop: true,
+  delay: function(el, i) { return i * 250 },
+});
+
+anime({
+  targets: '#outline6',
+  strokeDashoffset: [anime.setDashoffset, -3],
+   easing: 'easeInOutSine',
+  duration: 1000,
+  loop: true,
+  delay: function(el, i) { return i * 100 },
+});
+
+anime({
+  targets: '#outline7',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  loop: true,
+  delay: function(el, i) { return i * 250 },
+});
